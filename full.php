b3f3652608c4890e2680b474c19972477e1c1b03
JustinCredible Studios <tjustincredible219@gmail.com>
2025-03-07 16:39:08 -0500

    Got the front end talking to the back end of the application

--- full.php
+++ full.php
@@ -1,6 +1,18 @@
 <?php 
+include 'config.php';
 
+$conn = new mysqli($host, $username, $password, $dbname);
 
+$sql = "SELECT * FROM email_notififationsystem";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
 
 
 ?>
