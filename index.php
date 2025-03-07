91c2926edf10ab479e51eb758b33c51dcc3a1f9f
JustinCredible Studios <tjustincredible219@gmail.com>
2025-03-07 16:18:22 -0500

    Projtect Set up

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Email Notification System</title>
+    <link rel="stylesheet" type="text/css" href="wampthemes/modern/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+            <form style="margin-bottom:20px;" method="POST" action="full.php">
+                <span style="padding:10px;"><a class="btn btn-success active"  href="full.php">We are at Capacity</a></span>
+            </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
