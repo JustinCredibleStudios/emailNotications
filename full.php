8b6a4ded86c7cd3a8baae6e4e009ebd677a3f5a1
JustinCredible Studios <tjustincredible219@gmail.com>
2025-03-08 11:06:40 -0500

    reading data from database

--- full.php
+++ full.php
@@ -3,12 +3,12 @@ include 'config.php';
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
-$sql = "SELECT * FROM email_notififationsystem";
+$sql = "SELECT email FROM email_notififationsystem";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " " . $row["email"]. "<br>";
+        echo $row["email"]. "<br>";
     }
 } else {
     echo "0 results";
